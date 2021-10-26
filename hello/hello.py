9575a75a768fe8ce3bffa419136ba5fcaa642b13
Major Hayden <major@redhat.com>
2021-10-26 10:52:39 -0500

    Add date string to hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return f"Hello, World! The current date is: {datetime.now()}"
